889d28efea9110ad852bb439d1dd91c3110f7383
David Aspeslagh <aspe0004@hz.nl>
2018-09-12 21:41:09 +0200

    Added working build script

---
+++ Assets/Editor/EditorScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    [MenuItem("Build/Build Windows")]
+    public static void BuildWindows()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity" };
+        buildPlayerOptions.locationPathName = "/Build";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
